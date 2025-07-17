c3af2dd22a80d765082fa9471193f59773640b65
samuyang <samuyang@cisco.com>
2025-07-17 07:35:17 -0700

    feat: ui updates for identity integration
    Signed-off-by: samuyang <samuyang@cisco.com>

---
+++ coffeeAGNTCY/coffee_agents/lungo/exchange/frontend/src/components/MainArea/Badge.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { FaIdBadge } from 'react-icons/fa';
+import { FarmName } from '../../utils/const.js'; // Adjust the import path as necessary
+
+const Badge = ({ farmName }) => {
+    return (
+        <div>
+            <button
+                style={{
+                    padding: '10px',
+                    // Brazil Coffee Farm doesn't have valid badge, so we use a different color
+                    backgroundColor: farmName ===  FarmName.BrazilCoffeeFarm ? '#E63939' : '#187ADC',
+                    color: '#fff',
+                    border: 'none',
+                    borderRadius: '50%',
+                    cursor: 'pointer',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}
+            >
+                <FaIdBadge size={20} />
+            </button>
+        </div>
+    );
+};
+
+export default Badge;
