3293751620f0e8b6ad037025964b95043cba7c40
samuyang <samuyang@cisco.com>
2025-07-28 10:11:57 -0700

    chore: update api key
    Signed-off-by: samuyang <samuyang@cisco.com>

--- coffeeAGNTCY/coffee_agents/lungo/config/config.py
+++ coffeeAGNTCY/coffee_agents/lungo/config/config.py
@@ -20,8 +20,8 @@ ENABLE_HTTP = os.getenv("ENABLE_HTTP", "true").lower() in ("true", "1", "yes")
 
 # AGNTCY Identity Integration
 ## These API Keys are created in OSS environment and hardcoded for demo purposes.
-IDENTITY_VIETNAM_AGENT_SERVICE_API_KEY = os.getenv("IDENTITY_VIETNAM_AGENT_SERVICE_API_KEY", "80arL5Rpvy3IL14IYEo2;<wq9)2>Z!!r-:(40eUTX+E5NAp)(g)2b2@u;qS2S2z{")
-IDENTITY_COLOMBIA_AGENT_SERVICE_API_KEY = os.getenv("IDENTITY_COLOMBIA_AGENT_SERVICE_API_KEY", "D4tPQ>S+if24h5xX9y+@U1{PiZ6e-nmK0eG4h@5@@7,r<J-I:H5bjf9!-Z@59Z2J")
+IDENTITY_VIETNAM_AGENT_SERVICE_API_KEY = os.getenv("IDENTITY_VIETNAM_AGENT_SERVICE_API_KEY", "7h@x>00P}>qY<-RR49s3>d1vKb:J1,>!RS0w>d4ZXn:pU8MrO1]c+,70r3N6HPzg")
+IDENTITY_COLOMBIA_AGENT_SERVICE_API_KEY = os.getenv("IDENTITY_COLOMBIA_AGENT_SERVICE_API_KEY", "9y2K5faqRa0N]3[:3+1VZ8Jt04oG@+Mf1[0}ou7y+0QIO!z@6gQBl(W}+}-UkV7i")
 IDENTITY_API_KEY = os.getenv("IDENTITY_API_KEY", "n-;bYO5iMtdP7X787PP5G63E6e9O-6,U2paS@y+>-zv15Y[7{![l@@eg(8chS;LU")
 ## Endpoint to access Identity API. Refer to https://identity-docs.staging.outshift.ai/docs/api
 IDENTITY_API_SERVER_URL = os.getenv("IDENTITY_API_SERVER_URL", "https://api.identity-service.staging.outshift.ai")
