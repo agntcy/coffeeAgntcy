bc36f9045f1be6850e29e0c3d14d3bfcbb51af76
Chizuzu <izuzuchidumga@gmail.com>
2025-10-24 09:19:00 -0700

    remove unused prompts and correct existing prompts

--- coffeeAGNTCY/coffee_agents/lungo/frontend/src/components/Chat/Prompts/CoffeePromptsDropdown.tsx
+++ coffeeAGNTCY/coffee_agents/lungo/frontend/src/components/Chat/Prompts/CoffeePromptsDropdown.tsx
@@ -19,7 +19,6 @@ const CoffeePromptsDropdown: React.FC<CoffeePromptsDropdownProps> = ({
 
   const buyerPrompts = [
     "What yield do the farms have?",
-    "What is the current inventory?",
     "I'd like to buy 200 lbs quantity of coffee and who can fulfill it?",
     "What is the current inventory for Brazil farm?",
     "What is the current inventory for Colombia farm?",
