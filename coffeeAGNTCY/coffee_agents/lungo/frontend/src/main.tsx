b903a76998f29539b8483b76fb446482375d58b0
misi-bp <mkaloczy@cisco.com>
2026-03-13 18:09:45 +0100

    feat: add BrowserRouter provider
    Signed-off-by: misi-bp <mkaloczy@cisco.com>

--- coffeeAGNTCY/coffee_agents/lungo/frontend/src/main.tsx
+++ coffeeAGNTCY/coffee_agents/lungo/frontend/src/main.tsx
@@ -5,6 +5,7 @@
 
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
 import App from "./App"
 import "./index.css"
 
@@ -13,6 +14,8 @@ if (!rootElement) throw new Error("Failed to find the root element")
 
 createRoot(rootElement).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
