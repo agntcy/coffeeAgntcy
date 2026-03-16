ecea11ae6feb207ac39f9f52cb38bec9b8968c0e
misi-bp <mkaloczy@cisco.com>
2026-03-16 12:27:33 +0100

    chore: comment out the unimplemented pages' routes
    Signed-off-by: misi-bp <mkaloczy@cisco.com>

--- coffeeAGNTCY/coffee_agents/lungo/frontend/src/App.tsx
+++ coffeeAGNTCY/coffee_agents/lungo/frontend/src/App.tsx
@@ -5,15 +5,15 @@
 
 import React from "react"
 import { Routes, Route, Navigate } from "react-router-dom"
-import { RootPage, Workflow, Patterns, UseCases } from "@/pages"
+import { RootPage } from "@/pages"
 
 const App: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<RootPage />} />
-      <Route path="/agentic-workflows/:workflowId" element={<Workflow />} />
-      <Route path="/patterns" element={<Patterns />} />
-      <Route path="/use-cases" element={<UseCases />} />
+      {/* <Route path="/agentic-workflows/:workflowId" element={<Workflow />} /> */}
+      {/* <Route path="/patterns" element={<Patterns />} /> */}
+      {/* <Route path="/use-cases" element={<UseCases />} /> */}
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   )
