81fee041835e1f5dc9044370c42f96d17f5975a6
misi-bp <mkaloczy@cisco.com>
2026-09-04 10:07:15 +0200

    refactor(lungo): replace useElementSize with useElementHeight and elementSize type
    Signed-off-by: misi-bp <mkaloczy@cisco.com>

--- coffeeAGNTCY/coffee_agents/lungo/frontend/src/hooks/layout/index.ts
+++ coffeeAGNTCY/coffee_agents/lungo/frontend/src/hooks/layout/index.ts
@@ -3,8 +3,8 @@
  * SPDX-License-Identifier: Apache-2.0
  **/
 
+export type { ElementSize } from "./elementSize"
 export { useElementWidth } from "./useElementWidth"
-export { useElementSize } from "./useElementSize"
-export type { ElementSize } from "./useElementSize"
+export { useElementHeight } from "./useElementHeight"
 export { useElementRect } from "./useElementRect"
 export { useIsBelowSmBreakpoint } from "./useIsBelowSmBreakpoint"
